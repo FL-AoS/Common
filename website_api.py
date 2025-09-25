ce14c7d6fed3c51e444ca96f6753cb61283288da
DryByte <93609026+DryByte@users.noreply.github.com>
2025-09-25 10:12:35 -0300

    save map

--- website_api.py
+++ website_api.py
@@ -22,12 +22,19 @@ TOKEN = WB_CFG.option("token").get()
 
 LOGIN_ENDPOINT = URL+"/api/server/login/validate"
 HIGHSCORE_UPLOAD_ENDPOINT = URL+"/api/server/highscores/upload"
+SAVE_MAP_ENDPOINT = URL+"/api/server/map/create"
+
+MAP_HIGHSCORES_ENDPOINT = URL+"/api/highscores/"
 
 HEADERS = {
 	"Content-Type": 'application/json',
 	"Accept": 'application/json',
 	"Authorization": "Bearer "+TOKEN
 }
+
+@command("register")
+def register(p, user=None, password=None):
+	return "Want to register? Go to our website: "+URL
 
 @command("login")
 def login(p, user=None, passw=None):
@@ -63,6 +70,7 @@ def login(p, user=None, passw=None):
 
 	resp_obj = resp.json()
 	p.logged_user_id = resp_obj["id"]
+	p.logged_user_name = resp_obj["login"]
 
 	p.on_website_login()
 
@@ -71,6 +79,7 @@ def login(p, user=None, passw=None):
 def apply_script(protocol, connection, config):
 	class WebsiteConnection(connection):
 		logged_user_id = None
+		logged_user_name = None
 
 		def on_website_login(self):
 			pass
@@ -78,5 +87,32 @@ def apply_script(protocol, connection, config):
 	class WebsiteProtocol(protocol):
 		def upload_player_highscores(self, obj):
 			requests.post(HIGHSCORE_UPLOAD_ENDPOINT, headers=HEADERS, json=obj)
+
+		def get_map_highscores(self):
+			resp = requests.get(MAP_HIGHSCORES_ENDPOINT+self.map_info.name)
+
+			if not resp or resp.status_code != 200:
+				return False
+
+			return resp.json()
+
+		def save_parkour_map(self):
+			infos = {}
+			infos["name"] = self.map_info.name
+			infos["author"] = self.map_info.author
+			infos["description"] = self.map_info.description
+
+			infos["type"] = "line"
+			if (self.map_info.extensions and "parkour_3d_checkpoints" in self.map_info.extensions):
+				infos["type"] = "checkpoint"
+
+			infos["checkpoints"] = len(self.map_info.extensions["parkour_checkpoints"])
+
+			resp = requests.post(SAVE_MAP_ENDPOINT, headers=HEADERS, json=infos)
+
+			if not resp or resp.status_code != 200:
+				return False;
+
+			return resp.json();
 
 	return WebsiteProtocol, WebsiteConnection
